722acc8bd2ada41ea8908de9543fcc0a7953f56f
sdas4 <sdas4@binghamton,edu>
2018-11-16 13:07:54 -0500

    add content

--- work/prj4-sol/docs.js
+++ work/prj4-sol/docs.js
@@ -70,8 +70,8 @@ function setupRoutes(app) {
   }
     function redirectAdd(app) {
         return async function (req, res) {
-            const postValue = req.body && req.body.submit !== undefined;
-            if(postValue) {
+           /* const postValue = req.body && req.body.submit !== undefined;
+           if(postValue) {
                 let checkName = req.file.originalname;
                 let name = Path.basename(checkName, '.txt');
                 let content = req.file.buffer.toString('utf8');
@@ -79,7 +79,7 @@ function setupRoutes(app) {
                 console.log(result);
                 res.redirect(relativeUrl(req, `../${name}`));
                 return;
-            }
+            }*/
             const view = {base: app.locals.base};
             const html = doMustache(app, 'add', view);
             res.send(html);
